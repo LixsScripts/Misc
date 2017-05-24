7c08dc2caccb05998fcb9c9d9aa925f00de368d3
Luke Hodgson <l.c.hodgson.94@gmail.com>
2017-05-24 20:11:17 +0100

    Uploaded project.

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var randomNumber = this.GenerateRandomNumber(1, 18);
+
+            this.label1.Text = $"Random Number is {randomNumber}";
+        }
+
+        private int GenerateRandomNumber(int min, int max)
+        {
+            if (min >= max || max <= min)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "The parameter @min should be less than and not equal to the parameter @max" + 
+                    $"@min: {min}, @max:{max}");
+            }
+
+            return new Random().Next(min, max);
+        }
+    }
+}
